f20b2644b81cd97684adcf1d25551b4a38dd2c35
Kresimir Vlahov <kresimir.vlahov@infinum.com>
2022-01-20 14:05:00 +0100

    Add default keyword to setting

--- lib/log_book/configuration.rb
+++ lib/log_book/configuration.rb
@@ -1,10 +1,10 @@
 module LogBook
   extend Dry::Configurable
 
-  setting :records_table_name,    'records'
-  setting :ignored_attributes,    [:updated_at, :created_at]
-  setting :always_record,         false
-  setting :author_method,         :current_user
-  setting :record_squashing,      false
-  setting :skip_if_empty_actions, [:update]
+  setting :records_table_name,    default: 'records'
+  setting :ignored_attributes,    default: [:updated_at, :created_at]
+  setting :always_record,         default: false
+  setting :author_method,         default: :current_user
+  setting :record_squashing,      default: false
+  setting :skip_if_empty_actions, default: [:update]
 end
